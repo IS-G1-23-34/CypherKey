f40d5c8af783e4bf4068495ab0a4be8c038cce81
nachoalv <ignacioalvaneznunez@gmail.com>
2023-10-03 16:35:39 +0200

    ARRANQUE PROYECTO

---
+++ src/main/java/es/IS/CipherKey/CipherKeyApplication.java
@@ -0,0 +1,13 @@
+package es.IS.CipherKey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipherKeyApplication.class, args);
+	}
+
+}
