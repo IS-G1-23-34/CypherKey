14eee40522752547dfbd7ec133399727de629a95
nachoalv <ignacioalvaneznunez@gmail.com>
2023-10-10 15:41:48 +0200

    Clases iniciales

---
+++ src/main/java/es/IS/CipherKey/Comprobador.java
@@ -0,0 +1,18 @@
+package es.IS.CipherKey;
+
+public class Comprobador {
+    private String entrada;
+
+    public Comprobador(String entrada) {
+        this.entrada = entrada;
+    }
+
+    public boolean comprobadorContrasena(){
+        boolean esSeguro = false;
+
+
+
+
+        return esSeguro;
+    }
+}
---
+++ src/main/java/es/IS/CipherKey/Generador.java
@@ -0,0 +1,4 @@
+package es.IS.CipherKey;
+
+public class Generador {
+}
