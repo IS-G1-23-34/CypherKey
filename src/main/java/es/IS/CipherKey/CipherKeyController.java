681a6bbfbc1241be19c8b7779458d224bced2316
aalmeidar <a.almeida.2021@alumnos.urjc.es>
2023-10-11 13:12:29 +0200

    ISG12324-15 Avances con el nav
    Avance con el nav y alguna estética de la Web. Versión de prototipo. El fondo de particulas (particles2.js) es provisional porque el acordado (particles.json) no funciona correctamente.

--- src/main/java/es/IS/CipherKey/CipherKeyController.java
+++ src/main/java/es/IS/CipherKey/CipherKeyController.java
@@ -1,15 +1,16 @@
 package es.IS.CipherKey;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class CipherKeyController {
 
     @GetMapping("/")
     public String index() {
-
         return "index";
     }
 
