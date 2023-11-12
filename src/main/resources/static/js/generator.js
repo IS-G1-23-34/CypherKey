bee14cb02f0ab59e8e739d71638b5d20a90c3b00
nachoalv <ignacioalvaneznunez@gmail.com>
2023-11-12 20:26:32 +0100

    Better options generar.
    Nueva forma de seleccionar la longitud, con alerta de longitud menor a 8.

--- src/main/resources/static/js/generator.js
+++ src/main/resources/static/js/generator.js
@@ -13,13 +13,8 @@
 let checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
 //Para marcar la opcion que llegue
-function marcarOpcion(len, min, may, num, spe) {
-    let miSelect = document.getElementById("miSelect");
-    for (let i = 0; i < miSelect.options.length; i++) {
-        if (miSelect.options[i].value === len) {
-            miSelect.options[i].selected = true; //it checks the option
-        }
-    }
+function marcarOpcion(min, may, num, spe) {
+
     if (min === "true")
         document.getElementById("checkmin").checked = true;
     if (may === "true")
@@ -39,10 +34,42 @@ function validarUltimaCheckbox() {
             casillasActivadas++;
         }
     });
-    return casillasActivadas > 0;
+    if(casillasActivadas <= 0){
+        mostrarAlerta();
+        return false;
+    }
+    return true;
 }
 
+function mostrarAlerta() {
+    let alerta = document.getElementById('alertaNoCasillasActivas');
+    alerta.style.display = 'block';
+    setTimeout(function() {
+        alerta.style.display = 'none';
+    }, 3000); // Ocultar el toast después de 3 segundos
+}
 
+let miSelect = document.getElementById("selectNumero");
+
+function updateNumber(value) {
+    document.getElementById('slider').value = value;
+    alertaUnder8(value);
+}
+
+function updateSlider(value) {
+    document.getElementById('selectNumero').value = value;
+    alertaUnder8(value);
+}
+
+function alertaUnder8(value){
+    let alerta = document.getElementById('alertaUnder8');
+    if (value < 8)
+        alerta.style.display = 'block';
+        setTimeout(function() {
+            alerta.style.display = 'none';
+        }, 3000); // Ocultar el toast después de 3 segundos
+
+}
 //Funcion para copiar portapapeles
 document.getElementById("copyButton").addEventListener("click", function (){
     const password = document.getElementById("password");
